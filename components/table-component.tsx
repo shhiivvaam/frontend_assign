8061ae19fd41afe795a9eda358bf04424b69e214
Shivam Kumar <life.shivam2394@gmail.com>
2025-03-06 13:20:24 +0530

    creating default structure, adding user configs and removing false codes

---
+++ components/table-component.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React from "react"
+
+export type User = {
+    id: string
+    name: string
+    username: string
+    status: string
+    role: string
+    email: string
+    teams: string[]
+    age: number
+    avatar: string
+}
+
+export default function TableComponent() {
+    return (
+        <>
+        </>
+    );
+}
+
